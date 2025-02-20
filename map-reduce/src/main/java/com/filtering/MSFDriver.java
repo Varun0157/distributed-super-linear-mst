74e73f8393b37c27f0e00805efdac69db0f4c807
Varun0157 <varun.edachali@gmail.com>
2025-02-20 15:54:04 +0530

    feat: allow dynamic epsilon, input path, output prefix path

--- map-reduce/src/main/java/com/filtering/MSFDriver.java
+++ map-reduce/src/main/java/com/filtering/MSFDriver.java
@@ -1,56 +1,89 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
+import org.eclipse.jetty.util.IO;
 
 public class MSFDriver {
-  public static void main(String[] args) throws Exception {
-    // Base directory where data, and results reside
-    String basePath = "file:///home/varun.edachali/map-reduce/";
-    // Initial input is in the "data" directory.
-    String inputPath = basePath + "data";
-    int round = 0;
+  private static void deleteMetaDataFiles(String dirPath, String prefix) throws IOException {
+    File dir = new File(dirPath);
+    if (!dir.exists()) {
+      throw new IOException("directory does not exist: " + dirPath);
+    }
+    if (!dir.isDirectory()) {
+      throw new IOException("not a directory: " + dirPath);
+    }
 
-    // Create Hadoop configuration and filesystem objects.
+    File[] files = dir.listFiles((directory, fileName) -> fileName.startsWith(prefix));
+    if (files == null) {
+      throw new IOException("unable to list files in directory: " + dirPath);
+    }
+
+    for (File file : files) {
+      System.out.println("deleting " + file.getName());
+      if (file.delete()) {
+        continue;
+      }
+      throw new IOException("failed to delete " + file.getName());
+    }
+  }
+
+  private static void calculateMST(String inputDir, String outputPrefix, float epsilon) throws Exception {
+    final String basePath = "file:///home/varun.edachali/map-reduce/";
+    final String inputPath = basePath + inputDir;
+
     Configuration conf = new Configuration();
-    conf.set("fs.defaultFS", "file:///");
+    conf.set("fs.defaultFS", "file:///"); // use local file system instead of hdfs
     FileSystem fs = FileSystem.get(conf);
 
+    int round = 0;
     while (true) {
-      // List the files in the current input directory.
       FileStatus[] inputFiles = fs.listStatus(new Path(inputPath));
       int numFiles = inputFiles.length;
-      System.out.println("Round " + round + ": " + numFiles + " files found in " + inputPath);
+      System.out.println("round " + round + ": " + numFiles + " files found in " + inputPath);
 
-      // Stop if there is only one file remaining.
-      if (numFiles <= 2) { // some _SUCCESS file is also being pushed, fix later
-        System.out.println("Only one file remains. Terminating iterations.");
+      if (numFiles <= 1) {
+        System.out.println("only one file remains. Terminating iterations.");
         break;
       }
 
-      double reductionFactor = Math.pow(10, 1.1);
-      int numReducers = (int) Math.ceil((double) numFiles / reductionFactor);
-      String outputPath = basePath + "res_" + round;
-      System.out.println("Running job with " + numReducers + " reducers, output will be stored in " + outputPath);
+      final double reductionFactor = Math.pow(10, 1 + epsilon);
+      final int numReducers = (int) Math.ceil((double) numFiles / reductionFactor);
+      final String outputPath = basePath + outputPrefix + round;
+      System.out.println("running job with " + numReducers + " reducers, output will be stored in " + outputPath);
 
-      // Delete output directory if it already exists.
+      // the mapreduce invoked requires the output path to not exist
       Path outPath = new Path(outputPath);
       if (fs.exists(outPath)) {
         fs.delete(outPath, true);
       }
 
-      // Run the MinimumSpanningForest job.
-      // Note: The runMSF method should be updated to use the passed input and output
-      // paths.
       boolean success = MinimumSpanningForest.runMSF(inputPath, outputPath, numReducers);
       if (!success) {
-        System.err.println("Job failed in round " + round);
+        System.err.println("job failed in round " + round);
         System.exit(1);
       }
+      // goal: get rid of the _SUCCESSS files
+      deleteMetaDataFiles(outputPath, "_");
 
-      // Set next input to be the output from this round.
       inputPath = outputPath;
       round++;
     }
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.out.println("Usage: MSFDriver <input_dir> <output_prefix> <epsilon>");
+      System.exit(-1);
+    }
+    final String inputDir = args[0];
+    final String outputPrefix = args[1];
+    final float epsilon = Float.parseFloat(args[2]);
+
+    calculateMST(inputDir, outputPrefix, epsilon);
   }
 }
